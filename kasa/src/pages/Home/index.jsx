0260baef2e2c98fd48e5d06e2907faf92eb345dc
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-08-11 10:14:29 +0200

    Style: update Home

--- kasa/src/pages/Home/index.jsx
+++ kasa/src/pages/Home/index.jsx
@@ -1,11 +1,19 @@
 import Section1 from "../../components/Section1";
 import React from "react";
+import Gallery from "../../components/Gallery";
+import styled from "styled-components";
+
+const Main = styled.main`
+  width: 86.11%;
+  margin: auto;
+`;
 
 function Home() {
   return (
-    <main>
+    <Main>
       <Section1 />
-    </main>
+      <Gallery />
+    </Main>
   );
 }
 
