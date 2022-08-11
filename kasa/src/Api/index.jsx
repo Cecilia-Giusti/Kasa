e69332ef37e292adb51e5d7bc8dc861ad3dd74c0
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-08-11 10:12:28 +0200

    Feat: update fetch

---
+++ kasa/src/Api/index.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+export function useFetch(url) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (!url) return;
+
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setData(data);
+      } catch (err) {
+        console.log("L'erreur est ici !");
+      }
+    }
+    fetchData();
+  }, [url]);
+  return { data };
+}
